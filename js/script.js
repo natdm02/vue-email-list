f446e188e9537df5de5cbf3a1eeab656971dddd3
natdm02 <natalidimarino2002>
2024-03-19 15:29:20 +0100

    [add] generazione email

--- js/script.js
+++ js/script.js
@@ -1,14 +1,32 @@
-const { createApp } = Vue;
-
-createApp({
+const app = Vue.createApp({
   data() {
     return {
       message: "Hello!",
+      mail: [],
     };
   },
 
-  mounted() {
-    axios.get("").then((response) => {});
+  methods: {
+    generaMail() {
+      this.mail = [];
+      for (let i = 0; i < 10; i++) {
+        axios
+          .get("https://flynn.boolean.careers/exercises/api/random/mail")
+          .then((response) => {
+            this.mail.push(response.data.response);
+          })
+          .catch((error) => {
+            console.error('Si è verificato un errore:', error);
+          });
+      }
+    },
   },
-  
-}).mount("#app");
+
+  mounted() {
+    this.generaMail();
+  },
+});
+
+
+app.mount("#app");
+
